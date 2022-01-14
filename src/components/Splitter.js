2c47bc1172903201a5f86d962348b33f1d33eeda
Nataly Ortega <nath_0563@hotmail.com>
2022-01-14 14:42:18 -0500

    Changing the Splitter component to make it a function component and use react hooks

--- src/components/Splitter.js
+++ src/components/Splitter.js
@@ -1,58 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 import { FaUser, FaDollarSign } from "react-icons/fa"
 import TipSelector from "./TipSelector";
 import CopyRight from "./CopyRight";
 import ValuesCalculator from "./ValuesCalculator";
 
-export default class Splitter extends React.Component {
-  state = {
-    billTotal: "",
-    tipPercentage: "",
-    numberOfPeople: "",
+export default function Splitter() {
+  const [ billTotal, setBillTotal ] = useState("");
+  const [ tipPercentage, setTipPercentage ] = useState("");
+  const [ numberOfPeople, setNumberOfPeople ] = useState("");
+
+  const handleBillOnChange = (value) => {
+    setBillTotal(value)
   }
 
-  handleBillOnChange = (value) => {
-    this.setState({
-      billTotal: value
-    })
+  const handleNumberOfPeopleOnChange = (value) => {
+    setNumberOfPeople(value)
   }
 
-  handleNumberOfPeopleOnChange = (value) => {
-    this.setState({
-      numberOfPeople: value
-    })
+  const handleTipSelectorOnChange = (value) => {
+    setTipPercentage(value)
   }
 
-  handleTipSelectorOnChange = (value) => {
-    this.setState({
-      tipPercentage: value
-    })
-  }
-
-  render() {
-    const { billTotal, tipPercentage, numberOfPeople } = this.state
-
-    return (
-      <div className="splitterContainer">
-        <div className="splitter">
-          <div className="requestedInfo">
-            <Input 
-              value={billTotal} label={"Bill"} placeholder={0} icon={FaUser} 
-              onChange={this.handleBillOnChange} errorMessage="Can't be zero"
-              isValid={billTotal !== "0"}
-            />
-            <TipSelector title="Select Tip %" onChange={this.handleTipSelectorOnChange} />
-            <Input 
-              value={numberOfPeople} label={"Number of People"} placeholder={0} icon={FaDollarSign} 
-              onChange={this.handleNumberOfPeopleOnChange} errorMessage="Can't be zero"
-              isValid={numberOfPeople !== "0"}
-            />
-          </div>
-          <ValuesCalculator billTotal={Number(billTotal)} tipPercentage={Number(tipPercentage)} numberOfPeople={Number(numberOfPeople)}/>
+  return (
+    <div className="splitterContainer">
+      <div className="splitter">
+        <div className="requestedInfo">
+          <Input 
+            value={billTotal} label={"Bill"} placeholder={0} icon={FaUser} 
+            onChange={handleBillOnChange} errorMessage="Can't be zero"
+            isValid={billTotal !== "0"}
+          />
+          <TipSelector title="Select Tip %" onChange={handleTipSelectorOnChange} />
+          <Input 
+            value={numberOfPeople} label={"Number of People"} placeholder={0} icon={FaDollarSign} 
+            onChange={handleNumberOfPeopleOnChange} errorMessage="Can't be zero"
+            isValid={numberOfPeople !== "0"}
+          />
         </div>
-        <CopyRight />
+        <ValuesCalculator billTotal={Number(billTotal)} tipPercentage={Number(tipPercentage)} numberOfPeople={Number(numberOfPeople)}/>
       </div>
-    )
-  }
+      <CopyRight />
+    </div>
+  )
 }
